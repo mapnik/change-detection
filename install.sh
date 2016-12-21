1560b8c8c9a445661d92925d926ac09d34d89b08
Dane Springmeyer <dane@mapbox.com>
2016-12-20 16:19:36 -0800

    allow re-install

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ set -o pipefail
 # without bridge
 function install() {
     mkdir -p mapnik-versions/v$1
-    (cd mapnik-versions/v$1 && mkdir node_modules && npm install mapnik@$1)
+    (cd mapnik-versions/v$1 && mkdir -p node_modules && npm install mapnik@$1)
 }
 
 install $@
