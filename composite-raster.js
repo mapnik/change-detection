be009d1577e083935fe3fa341fa5d657eb8fbe56
Dane Springmeyer <dane@mapbox.com>
2016-12-20 16:22:47 -0800

    rename, reorg composite test

--- composite-raster.js
+++ composite-raster.js
@@ -4,7 +4,7 @@ module.exports = function(mapnik_path) {
     var mapnik = require(mapnik_path);
     mapnik.register_default_input_plugins();
     var vt = new mapnik.VectorTile(15,30300,18986);
-    vt.setData(fs.readFileSync('./image.mvt'));
+    vt.setData(fs.readFileSync('./data/image.mvt'));
     var vt_target = new mapnik.VectorTile(18,242403,151893,{buffer_size:0});
     console.time('comp');
     vt_target.composite([vt],{buffer_size:0,strictly_simple:false});
